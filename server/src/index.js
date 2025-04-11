e0cd1257f7bafca5fe3d2c7bd0d8a7a91e210a21
ShadowKnSs <xavirohe123@hotmail.com>
2025-04-11 17:04:57 -0600

    Preparacion del Entono

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', require('./routes/api'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
